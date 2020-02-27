a348a7cfa8f46666dfaa2a0ea0753288c8e86b6e
Kabilan <53993282+Kabilan21@users.noreply.github.com>
2020-02-27 15:36:20 +0530

    Add files via upload

---
+++ prority scheduling.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int n=4;
+    int bt[n],wt[n],tat[n],i,avgtat=0,avgwt=0,pt[n];
+    for(i=0;i<n;i++)
+    {
+        cout<<"Enter burst time:";
+        cin>>bt[i];
+        cout<<"Enter priority:";
+        cin>>pt[i];
+    }
+    int temp,j;
+    for(i=0;i<n-1;i++)
+        for(j=0;j<n-1;j++)
+    {
+      if(pt[j]>pt[j+1])
+      {
+          temp = bt[j];
+          bt[j] = bt[j+1];
+          bt[j+1]=temp;
+      }
+
+    }
+    wt[0]=0;
+    for(i=1;i<n;i++)
+    {
+        wt[i]=bt[i-1]+ wt[i-1];
+
+    }
+    for(i=0;i<n;i++)
+    {
+        tat[i]=wt[i]+bt[i];
+        avgtat+=tat[i];
+        avgwt+=wt[i];
+    }
+    cout<<"Process\tburst time\tWaititng_time\tturn around_time\n";
+
+    for(i=0;i<n;i++)
+    {
+        cout<<i<<'\t'<<bt[i]<<'\t'<<wt[i]<<'\t'<<tat[i]<<'\n';
+
+    }
+    cout<<"the average waititng time and turn around time are:"<<avgwt/4<<" "<<avgtat/4;
+}
